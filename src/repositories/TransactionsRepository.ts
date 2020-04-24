1c5cac91a44dd1283a3b78b00bf1884106fbe316
Bruno Simplicio <brunoraphael89@gmail.com>
2020-04-24 18:28:16 -0300

    react-native-project

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,67 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = { income: 0, outcome: 0, total: 0 };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    // const income = this.transactions
+    //   .filter(({ type }) => type === 'income')
+    //   .reduce((acc, curr, index, arr) => {
+    //     acc + curr.value;
+    //     return acc;
+    //   }, 0);
+    // const outcome = this.transactions
+    //   .filter(({ type }) => type === 'outcome')
+    //   .reduce((acc, curr, index, arr) => {
+    //     acc + curr.value;
+    //     return acc;
+    //   }, 0);
+
+    // const total = income - outcome;
+
+    // const balance = {
+    //   income,
+    //   outcome,
+    //   total,
+    // };
+
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public setBalance(type: string, value: number): void {
+    if (type === 'income') {
+      this.balance.income += value;
+      this.balance.total += value;
+    } else {
+      this.balance.outcome += value;
+      this.balance.total -= value;
+    }
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.setBalance(type, value);
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
